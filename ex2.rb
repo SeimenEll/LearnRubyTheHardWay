76e6abc86f2388c92c190b34e012884ac5827e64
Simon Liebelt <si.liebelt@gmail.com>
2018-02-11 12:06:21 +0100

    Added comments that explain the order in which the calculation is done.

--- ex2.rb
+++ ex2.rb
@@ -1,14 +1,16 @@
 puts "I will now count my chickens:"
+#Punkt vor Strich
 puts "Hens #{25 + 30 / 6}"
+#Multiplikation vor Modulo vor Subtraktion 100-((25*3)%4)
 puts "Roosters #{100 - 25 * 3 % 4}"
 
 puts "Now i will count the eggs:"
 
-
+# 3+2+1-5+(4%2)-(1/4)+6
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 puts "Is it true that 3 + 2 < 5 - 7 ?"
-
+#Strich vor Logik
 puts 3 + 2 < 5 - 7
 
 puts "What is 3 + 2? #{3 + 2}"
